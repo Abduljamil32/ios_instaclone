fa6e318150485bba125b91ecfdfbf3be66ab392d
co.abu <anizomxoja@gmail.com>
2022-06-09 11:45:52 +0500

    Initial Commit

---
+++ ios_instaclone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_instaclone
+//
+//  Created by Avaz Mukhitdinov on 09/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_instaclone/ios_instacloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_instacloneApp.swift
+//  ios_instaclone
+//
+//  Created by Avaz Mukhitdinov on 09/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_instacloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
